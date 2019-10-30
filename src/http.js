414b47576d46f68c2e784dfc62811aa9e4cc0af7
fernando <rluis4490@gmail.com>
2019-10-29 18:30:07 -0600

    using callback

--- src/http.js
+++ src/http.js
@@ -1,4 +1,24 @@
 import {add} from './helpers';
 
 
+add.li('Line 4');
 
+function callback(message){
+    add.li(message);
+}
+
+function greeting(message,cb){
+    let start = Date.now();
+    for (let i = 0; i < 1000000000; i++) {
+        //nothing
+    }
+    add.li(`took: ${Date.now() - start}ms`);
+    cb(message)
+}
+
+greeting('Hello from Line 19',callback);
+
+add.li('Line 21');
+
+
+add.li('Line 24');
