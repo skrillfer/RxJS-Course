650223280e8d5fe9fa93eb026cfc7dec9394e168
fernando <rluis4490@gmail.com>
2019-10-30 11:16:40 -0600

    now create a async function with await

--- src/http.js
+++ src/http.js
@@ -3,20 +3,28 @@ import {resolve} from 'path';
 
 add.li('Line 4');
 
-const p = new Promise(
-    (resolve,reject)=>{
-        setTimeout(
-            () => {
-                resolve('We are complete!');
-            }, 5000
-        );
-    }
-);
+async function runPromise(){
+    add.li('Line 7');
+    const p = new Promise(
+        (resolve,reject)=>{
+            setTimeout(
+                () => {
+                    resolve('We are complete!');
+                }, 5000
+            );
+        }
+    );
+    const message = await p;
+    add.li(message);
+    add.li('Line 18');
+}
 
-p.then(
-    message=>add.li(message)
-);
-add.li('Line 19');
+runPromise();
 
+add.li('Line 24');
 
-add.li('Line 22');
+/*
+await solo funciona dentro de la funcion async,
+si se desea esperar la ejecucion de runPromise() basta con
+runPromise(p=>add.li('Line 24'));
+ */
