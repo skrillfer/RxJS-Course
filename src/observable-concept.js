f24b6e2cd477295c11039137ac282aa372db2dae
fernando <rluis4490@gmail.com>
2019-10-30 21:04:14 -0600

    observer Object and using outside from observable

--- src/observable-concept.js
+++ src/observable-concept.js
@@ -1,48 +1,16 @@
 import {add} from './helpers';
-import {Observable, observable } from 'rxjs';
-//Overview of Observables
 
-//Create a Observable
-const o = new Observable(
-    (observer)=>{
-        setInterval(
-            () => {
-                observer.next('Observable every second');
-            }, 1000
-        );
-    }
-);
-//Create a Promise
-const p = new Promise(
-    (resolve,reject)=>{
-        setTimeout(
-            () => {
-                resolve('Promise');
-            }, 1000
-        );
-    }
-);
+function Observable(subscriber){
+    subscriber.next('Hello Universe!');
+}
 
-//Call Promise
-p.then(
-    message=>{
-        add.li(message);
+const observer = {
+    next: add.li,
+    error: add.li,
+    complete: ()=>{
+        add.li('There are no more values');
     }
-);
+}
 
-//Call Observable  subscribe(next,error,complete)
-const subscription = o.subscribe(
-    {
-        next: (message)=>{
-            add.li(message);
-        },
-        error: (error)=> console.error(error),
-        complete: ()=> add.li('This observable is complete!!')
-    }
-);
-
-setTimeout(
-    ()=>{
-        subscription.unsubscribe();
-    },4000
-);
+observer.next("Some Value");
+observer.complete();
